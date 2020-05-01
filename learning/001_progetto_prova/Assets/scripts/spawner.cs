fd6e86fc1a193a740a8ef8a98441e3bd3121c28e
Luca Bruno <luca.bru@protonmail.com>
2020-05-01 13:04:45 +0200

    gameObject and prefab

---
+++ learning/001_progetto_prova/Assets/scripts/spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;  //unity library
+
+public class spawner : MonoBehaviour
+{
+    [SerializeField]    //ispactor can see this
+    GameObject cubePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("sono spawnato>> " + gameObject.name, gameObject);    //linked to associated game object
+        Instantiate(cubePrefab);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))   //0 = left clicke released
+        {
+            Instantiate(cubePrefab, new Vector3(Random.Range(-5,5), Random.Range(-5, 5), Random.Range(-5, 5)), cubePrefab.transform.rotation);
+        }
+    }
+}
