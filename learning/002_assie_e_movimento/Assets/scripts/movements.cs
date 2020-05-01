fad5c1e09847ae733e730b21e624078a0c68d45c
Luca Bruno <luca.bru@protonmail.com>
2020-05-01 14:49:56 +0200

    Collider and Movements

---
+++ learning/002_assie_e_movimento/Assets/scripts/movements.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movements : MonoBehaviour
+{
+
+    float speed = 10;
+    
+    void Update()
+    {
+        float horizontalMovements = Input.GetAxis("Horizontal");
+        float verticalMovements = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(0, 0, 0);   //new vector for movements traslation
+
+        //check Horizontal
+        if (horizontalMovements != 0)
+        {
+            direction.x = horizontalMovements;
+        }
+
+        //check Vertical
+        if (verticalMovements != 0)
+        {
+            direction.y = verticalMovements;
+        }
+
+        transform.Translate(direction * (speed * Time.deltaTime));    //move object
+    }
+}
